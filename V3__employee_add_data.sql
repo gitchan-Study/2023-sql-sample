b33e15111e4241bf5512585e280f9a70ce29cb32
gitchan <eunkeeeee@gmail.com>
2023-12-01 12:47:40 +0900

    feat: employee 데이터 삽입

---
+++ V3__employee_add_data.sql
@@ -0,0 +1,22 @@
+insert into employee
+values (1, 'MESSI', '1987-02-01', 'M', 'DEV_BACK', 100000000, null)
+
+insert into employee
+values (2, 'JANE', '1996-05-05', 'F', 'DSGN', 90000000, null);
+
+insert into employee (id, name, birth_date, sex, position)
+values (3, 'JENNY', '2000-10-12', 'F', 'DEV_BACK');
+
+insert into employee
+values (4, 'BROWN', '1996-03-13', 'M', 'CEO', 120000000, null),
+       (5, 'DINGYO', '1990-11-05', 'M', 'CTO', 120000000, null),
+       (6, 'JULIA', '1986-12-11', 'F', 'CFO', 120000000, null),
+       (7, 'MINA', '1993-06-17', 'F', 'DSGN', 80000000, null),
+       (8, 'JOHN', '1999-10-22', 'M', 'DEV_FRONT', 65000000, null),
+       (9, 'HENRY', '1982-05-20', 'M', 'HR', 82000000, null),
+       (10, 'NICHOLE', '1991-03-26', 'F', 'DEV_FRONT', 90000000, null),
+       (11, 'SUZANNE', '1993-03-23', 'F', 'PO', 75000000, null),
+       (12, 'CURRY', '1998-01-15', 'M', 'PLN', 85000000, null),
+       (13, 'JISUNG', '1989-07-07', 'M', 'PO', 90000000, null),
+       (14, 'SAM', '1992-08-05', 'M', 'DEV_INFRA', 70000000, null);
+
